b9b875d0e6191666561359623ecab6944f881fd0
Muhammet Erol <muhammederol971@gmail.comgit config --global user.email muhammederol971@gmail.com>
2024-06-28 02:05:30 +0200

    added customer slider.

--- src/components/customers/Customers.jsx
+++ src/components/customers/Customers.jsx
@@ -1,14 +1,62 @@
+"use client"
 import React from "react";
 import CustomerItem from "./CustomerItem";
-
+import Slider from "react-slick";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 const Customers = () => {
+  function NextBtn({ onClick }) {
+    return (
+      <button
+        className="absolute -bottom-12 left-1/2 bg-primary flex items-center justify-center w-10 h-10 rounded-full text-white"
+        onClick={onClick}
+      >
+        <IoIosArrowForward />
+      </button>
+    );
+  }
+
+  function PrevBtn({ onClick }) {
+    return (
+      <button
+        className="absolute -bottom-12 right-1/2 bg-primary flex items-center justify-center w-10 h-10 rounded-full text-white mr-2"
+        onClick={onClick}
+      >
+        <IoIosArrowBack />
+      </button>
+    );
+  }
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    arrows: true,
+    nextArrow: <NextBtn />,
+    prevArrow: <PrevBtn />,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <div className="container mx-auto my-20">
-      <div addClass="text-[40px] text-center">What Says Our Customers About Us</div>
-      <div className="flex gap-x-10">
-        <CustomerItem imgSrc="/images/burger-sunum-yemekcom.jpg"/>
-        <CustomerItem imgSrc= "/images/burger-sunum-yemekcom.jpg" />
-      </div>
+      <div className="text-[40px] text-center">What Says Our Customers About Us</div>
+      <Slider {...settings}>
+        <CustomerItem imgSrc="/images/burger-sunum-yemekcom.jpg" />
+        <CustomerItem imgSrc="/images/burger-sunum-yemekcom.jpg" />
+        <CustomerItem imgSrc="/images/burger-sunum-yemekcom.jpg" />
+        <CustomerItem imgSrc="/images/burger-sunum-yemekcom.jpg" />
+   
+      </Slider>
     </div>
   );
 };
