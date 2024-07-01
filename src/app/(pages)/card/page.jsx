adcb1f57be60d0fb1c965c94a06a862aaeaddbcc
Muhammet Erol <muhammederol971@gmail.comgit config --global user.email muhammederol971@gmail.com>
2024-07-01 18:32:01 +0200

    styled card comp

--- src/app/(pages)/card/page.jsx
+++ src/app/(pages)/card/page.jsx
@@ -2,51 +2,67 @@ import Image from "next/image";
 
 const Card = () => {
   return (
-    <div className="min-h-[calc(100vh_-_433px)]"> 
-      <div className="flex justify-between items-center">
-        <div className="min-h-[calc(100vh_-_433px)] flex items-center flex-1">
-          <table className="table-auto w-full"> 
-            <thead>
+    <div className="min-h-[calc(100vh_-_433px)]">
+      <div className="flex justify-between items-center md:flex-row flex-col">
+        <div className="md:min-h-[calc(100vh_-_433px)] flex items-center flex-1 p-10 overflow-x-auto w-full">
+          <table className="w-full text-sm text-center text-gray-500 min-w-[1000px]">
+            <thead className="text-xs text-gray-400 uppercase bg-gray-700">
               <tr>
-                <th className="px-4 py-2">PRODUCT</th> 
-                <th className="px-4 py-2">EXTRAS</th>
-                <th className="px-4 py-2">PRICE</th>
-                <th className="px-4 py-2">QUANTITY</th>
+                <th scope="col" className="py-3 px-6">
+                  PRODUCT
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  EXTRAS
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  PRICE
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  QUANTITY
+                </th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td className="px-4 py-2 flex items-center"> 
+              <tr className="transition-all bg-secondary border-gray-700 hover:bg-primary">
+                <td className="py-5 px-6 font-medium whitespace-nowrap hover:text-white flex items-center gap-x-1 justify-center">
                   <Image
                     src="/images/burger-sunum-yemekcom.jpg"
                     alt="Burger Image"
                     width={50}
                     height={50}
-                    className="mr-4" 
+                    className="mr-4"
                   />
-                  <span>Good Pizza</span>
+                  <span>Delicious Pizza</span>
                 </td>
-                <td className="px-4 py-2">
-                  <span>mayonez, spicy, sauce, ketchup</span> 
+                <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
+                  <span>mayonez, spicy, sauce, ketchup</span>
                 </td>
-                <td className="px-4 py-2">$20</td>
-                <td className="px-4 py-2">1</td>
+                <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
+                  $20
+                </td>
+                <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
+                  1
+                </td>
               </tr>
             </tbody>
           </table>
         </div>
-        <div className="bg-secondary min-h-[calc(100vh_-_433px)] flex flex-col justify-center text-white p-12">
-          <div className="text-[40px]">CART TOTAL</div> 
+        <div className="bg-secondary min-h-[calc(100vh_-_433px)] flex flex-col justify-center text-white p-12 md:w-auto w-full   md:text-start !text-center">
+          <div className="text-[40px]">CART TOTAL</div>
           <div className="mt-6">
-            <b>Subtotal: </b>$20 <br /> 
+            <b>Subtotal: </b>$20 <br />
             <b className="inline-block my-1">Discount: </b>$0.00 <br />
-            <b>Total: </b>$20 
+            <b>Total: </b>$20
           </div>
-          <button className="btn-primary mt-4">CHECKOUT NOW!</button>
+          <div>
+            <button className="btn-primary mt-4 md:w-auto w-52">
+              CHECKOUT NOW!
+            </button>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default Card; // Card bileşenini dışa aktarır
+export default Card; 
